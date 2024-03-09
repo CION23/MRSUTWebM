da2785e2211f4ec7e0e2f3cd1ed42298e41a2ac2
Ion Cegolea <ioncegolea23@gmail.com>
2024-03-09 16:11:24 +0200

    Add project files.

---
+++ eUseControl/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Optimization;
+
+namespace eUseControl.App_Start
+{
+     public class BundleConfig
+     {
+          public static void RegisterBundles(BundleCollection bundles)
+          {
+               bundles.Add(new StyleBundle("~/bundles/bootstrap/css")
+                         .Include("~/Content/bootstrap.css", new CssRewriteUrlTransform()));
+
+               bundles.Add(new StyleBundle("~/bundles/Site/css")
+                         .Include("~/Content/Site.css", new CssRewriteUrlTransform()));
+
+               bundles.Add(new StyleBundle("~/bundles/fontawesome/css")
+                         .Include("~/Content/all.css", new CssRewriteUrlTransform()));
+
+               bundles.Add(new ScriptBundle("~/bundles/bootstrap/js")
+                         .Include("~/Scripts/bootstrap.js"));
+
+               bundles.Add(new ScriptBundle("~/bundles/bootstrap/js")
+                         .Include("~/Scripts/jquery{version}.js"));
+          }
+     }
+}
