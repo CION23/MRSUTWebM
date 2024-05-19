6c2f9afe1000a887fb8614d7f41a3975c99fa314
CION <ioncegolea23@gmail.com>
2024-05-19 20:11:40 +0300

    login+++

--- eUseControl.BusinessLogic/DBModel/SessionContext.cs
+++ eUseControl.BusinessLogic/DBModel/SessionContext.cs
@@ -10,7 +10,7 @@ namespace eUseControl.BusinessLogic.DBModel
 {
      public class SessionContext : DbContext
      {
-          public SessionContext() : base("name=CCToolShop")
+          public SessionContext() : base("name=MVCMRSUTWEBDB")
           {
           }
 
